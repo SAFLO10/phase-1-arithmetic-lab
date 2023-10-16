ba4f16e7310cbcebc83b9786a0eb255e98251054
Sebastian <seb154@live.com>
2023-10-16 18:14:14 -0400

    modifies index.js

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 62
+let num2 = 1 
 
-// Write your code here
+let multiply = num1 * num2 
+
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 20;
+let num4 = 4;
+let mod = (num3 % num4) + 4;
+
+const max = Math.max(1,2,3,4,20);
+
